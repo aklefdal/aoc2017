49c7db573e06cbcf8f1783ef506e1b357f43f4a3
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2017-12-15 21:59:05 +0100

    Day 2 part 2

--- Day02/solve.fsx
+++ Day02/solve.fsx
@@ -3,16 +3,39 @@ let inputFile = Path.Combine(__SOURCE_DIRECTORY__, "input.txt")
 
 let parseLineToInts (line:string) =
     line.Split([|'\t'|]) 
-    |> Array.map int
+    |> Array.toList
+    |> List.map int
 
 let numbers = 
     File.ReadAllLines inputFile
-    |> Array.map parseLineToInts
+    |> Array.toList
+    |> List.map parseLineToInts
 
-let maxMinDiff (a: int array) =
-    let max = a |> Array.max
-    let min = a |> Array.min
+let maxMinDiff (a: int list) =
+    let max = a |> List.max
+    let min = a |> List.min
     max - min
 
 numbers 
-|> Array.sumBy maxMinDiff
+|> List.sumBy maxMinDiff
+
+// Part 2
+let rec findNumbersDivisibleWith e l = // assume sorted list, with largest first
+    match l with
+    | [] -> None
+    | h::t -> 
+        match e % h with
+        | 0 -> Some (e, h)
+        | _ -> findNumbersDivisibleWith e t
+
+let rec findDivisibleNumbers l =
+    match l with
+    | [] -> None
+    | h::t ->
+        match findNumbersDivisibleWith h t with
+        | Some (a, b) -> Some (a / b)
+        | None -> findDivisibleNumbers t 
+
+numbers
+|> List.map (List.sortDescending >> findDivisibleNumbers)
+|> List.sumBy Option.get
