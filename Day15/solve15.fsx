1330e58c457de28378928913c471f867eaade631
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2017-12-16 02:32:43 +0100

    Day 15 part 2

--- Day15/solve15.fsx
+++ Day15/solve15.fsx
@@ -32,3 +32,40 @@ let countEquals p =
 
 Seq.zip (genANext aStart count) (genBNext bStart count) 
 |> countEquals
+
+
+// Part 2
+
+let rec genA4Next (p:int64) (count:int) = seq { 
+    let rec innerGenA p =
+        let m = p * 16807L
+        let n = m % 2147483647L
+        match n % 4L with
+        | 0L -> n
+        | _ -> innerGenA n
+
+    let n = innerGenA p
+    yield n
+    if count > 1 then 
+        yield! genA4Next n (count - 1)
+}
+
+let rec genB8Next (p:int64) (count:int) = seq { 
+    let rec innerGenB p =
+        let m = p * 48271L
+        let n = m % 2147483647L
+        match n % 8L with
+        | 0L -> n
+        | _ -> innerGenB n
+
+    let n = innerGenB p
+    yield n
+    if count > 1 then 
+        yield! genB8Next n (count - 1)
+}
+
+let count2 = 5 * 1000 * 1000
+
+Seq.zip (genA4Next aStart count2) (genB8Next bStart count2) 
+|> countEquals
+
