c050c062eaf74b3820b1fca8edc9c24b351c5333
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2017-12-23 13:32:29 +0100

    Day 5, step 2

--- Day05/solve05.fsx
+++ Day05/solve05.fsx
@@ -1,6 +1,6 @@
 open System.IO
 
-let a =
+let input () =
     Path.Combine(__SOURCE_DIRECTORY__, "input05.txt")
     |> File.ReadAllLines
     |> Array.map int
@@ -14,6 +14,9 @@ type State = {
 let increment (a:int array) (c:int) =
     a.[c] <- a.[c] + 1
 
+let decrement (a:int array) (c:int) =
+    a.[c] <- a.[c] - 1
+
 let getNewState state = 
     let nextCurrent = state.current + state.a.[state.current]
     let updatedSteps = state.steps + 1
@@ -21,7 +24,7 @@ let getNewState state =
     {state with steps=updatedSteps;current=nextCurrent}
 
 let startState = {
-    a = a
+    a = input()
     steps = 0
     current = 0
 }
@@ -34,3 +37,29 @@ let rec move state =
     | false -> nextState |> move 
 
 startState |> move
+
+// Step 2
+
+let getNewStateStep2 state = 
+    let nextCurrent = state.current + state.a.[state.current]
+    let updatedSteps = state.steps + 1
+    match state.a.[state.current] >= 3 with
+    | true -> decrement state.a state.current
+    | false -> increment state.a state.current
+    {state with steps=updatedSteps;current=nextCurrent}
+
+let startStateStep2 = {
+    a = input()
+    steps = 0
+    current = 0
+}
+
+let lengthOfA2 = startStateStep2.a |> Array.length  
+
+let rec moveStep2 state =
+    let nextState = state |> getNewStateStep2
+    match nextState.current >= lengthOfA2 with
+    | true -> nextState.steps
+    | false -> nextState |> moveStep2
+
+startStateStep2 |> moveStep2
