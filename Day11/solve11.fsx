933a80a000d1abe9b70d9852a7acf6c7f3f901bd
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2017-12-29 22:11:41 +0100

    Refactored to ease part 2

--- Day11/solve11.fsx
+++ Day11/solve11.fsx
@@ -55,18 +55,65 @@ let simplify_NE_NW_N_Movements (tm:TotalMovement) =
         {tm with NE = tm.NE - steps; NW = tm.NW - steps; N = tm.N + steps}
     else
         tm
+let simplify_SE_SW_S_Movements (tm:TotalMovement) =
+    if tm.SE > 0 && tm.SW > 0 then
+        let steps = System.Math.Min(tm.SE, tm.SW)
+        {tm with SE = tm.SE - steps; SW = tm.SW - steps; S = tm.S + steps}
+    else
+        tm
+
+let simplify_NE_S_SE_Movements (tm:TotalMovement) =
+    if tm.NE > 0 && tm.S > 0 then
+        let steps = System.Math.Min(tm.NE, tm.S)
+        {tm with NE= tm.NE - steps; S = tm.S - steps; SE = tm.SE + steps}
+    else
+        tm
+
+let simplify_NW_S_SW_Movements (tm:TotalMovement) =
+    if tm.NW > 0 && tm.S > 0 then
+        let steps = System.Math.Min(tm.NW, tm.S)
+        {tm with NW= tm.NW - steps; S = tm.S - steps; SE = tm.SE + steps}
+    else
+        tm
+
+let simplify_NW_SW_N_Movements (tm:TotalMovement) =
+    if tm.SW > 0 && tm.N > 0 then
+        let steps = System.Math.Min(tm.SW, tm.N)
+        {tm with SW= tm.SW - steps; N = tm.N - steps; NW = tm.NW + steps}
+    else
+        tm
+
+let simplify_NE_SE_N_Movements (tm:TotalMovement) =
+    if tm.SE > 0 && tm.N > 0 then
+        let steps = System.Math.Min(tm.SE, tm.N)
+        {tm with SE = tm.SE - steps; N = tm.N - steps; NE = tm.NE + steps}
+    else
+        tm
+
+let simplify = 
+    simplify_N_S_Movements
+    >> simplify_NE_SW_Movements
+    >> simplify_NW_SE_Movements
+    >> simplify_NE_NW_N_Movements
+    >> simplify_SE_SW_S_Movements
+    >> simplify_NE_S_SE_Movements
+    >> simplify_NW_S_SW_Movements
+    >> simplify_NW_SW_N_Movements
+    >> simplify_NE_SE_N_Movements
 
 let summarize (tm:TotalMovement) = tm.N + tm.NE + tm.SE + tm.S + tm.SW + tm.NW 
 
-Path.Combine(__SOURCE_DIRECTORY__, "input11.txt")
-|> File.ReadAllText
-|> splitOnComma
-|> Array.toList
+let readInput =
+    Path.Combine(__SOURCE_DIRECTORY__, "input11.txt")
+    |> File.ReadAllText
+    |> splitOnComma
+    |> Array.toList
+
+readInput
 |> List.countBy id
 |> List.map (fun (direction, steps) -> {direction = direction;steps = steps })
 |> findTotalMovement
-|> simplify_N_S_Movements
-|> simplify_NE_SW_Movements
-|> simplify_NW_SE_Movements
-|> simplify_NE_NW_N_Movements
+|> simplify
 |> summarize
+
+// Part 2
