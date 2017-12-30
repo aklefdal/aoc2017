6c91a9eeb8de579c94e633b8714c9a77de0a1956
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2017-12-30 15:19:01 +0100

    Day 13 step 1

--- Day13/Solve13.fsx
+++ Day13/Solve13.fsx
@@ -9,8 +9,25 @@ let lineToInput (s:string) =
     let ss = s.Split([|':'|]) |> Array.map int
     {layer = ss.[0]; range = ss.[1]}
 
+let modFactor (range:int) =
+    match range with
+    | 1 -> 1
+    | _ -> (range - 1) * 2
+
+let isCaught (i:Input) =
+    match i.layer % (i.range |> modFactor) with
+    | 0 -> true
+    | _ -> false
+
+let calculateFactor (i:Input) =
+    match i |> isCaught with
+    | false -> 0
+    | true -> i.layer * i.range
+
 let input = 
-    Path.Combine(__SOURCE_DIRECTORY__, "sample13.txt")
-    //Path.Combine(__SOURCE_DIRECTORY__, "input13.txt")
+    // Path.Combine(__SOURCE_DIRECTORY__, "sample13.txt")
+    Path.Combine(__SOURCE_DIRECTORY__, "input13.txt")
     |> File.ReadAllLines
     |> Array.map lineToInput
+
+input |> Array.map calculateFactor |> Array.sum
